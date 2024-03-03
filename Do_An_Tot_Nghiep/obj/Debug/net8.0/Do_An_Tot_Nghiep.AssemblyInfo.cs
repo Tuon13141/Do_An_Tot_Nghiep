7bf039acd8fd73bd5d8a2edac9bb50d4e7efd852
Tuon13141 <ngteemo22@gmail.com>
2024-03-03 21:44:24 +0700

    Update 2

--- Do_An_Tot_Nghiep/obj/Debug/net8.0/Do_An_Tot_Nghiep.AssemblyInfo.cs
+++ Do_An_Tot_Nghiep/obj/Debug/net8.0/Do_An_Tot_Nghiep.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Do_An_Tot_Nghiep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd2ea11257202e21a6baa1417f7a4d615bbc3852")]
 [assembly: System.Reflection.AssemblyProductAttribute("Do_An_Tot_Nghiep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Do_An_Tot_Nghiep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
